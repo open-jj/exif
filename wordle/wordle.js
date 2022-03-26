f56d016ada733e19832a5c357a72bc62a03282ec
vklyucheno <78449629+vklyucheno@users.noreply.github.com>
2022-03-25 17:00:40 -0700

    Update wordle.js

--- wordle/wordle.js
+++ wordle/wordle.js
@@ -4,7 +4,7 @@ const f = Date.now();
 const h = f - g;
 const j = h / 86400000;
 const o = Math.round(j);
-const k = o + 1;
+const k = o + 2;
 
 let xmlHttpReq = new XMLHttpRequest();
 xmlHttpReq.open("GET", 'https://raw.githubusercontent.com/vklyucheno/tools/main/wordle/words.txt', false); 
